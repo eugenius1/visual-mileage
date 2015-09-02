960307d5f9151d6bfd7e7cb025fc6294dd642dd1
Eusebius N. <eugenius1@users.noreply.github.com>
2015-09-02 16:03:51 +0100

    clear_entries, add_line
    Buttons to call the above functions.
    Also use of add_entry function

--- visual-mileage.py
+++ visual-mileage.py
@@ -3,7 +3,6 @@
 # Purpose:     opens multiple Google maps with driving directions for given start-destination pairs
 #
 # Author:      github.com/eugenius1
-#
 # Created:     01/09/2015
 # Copyright:   (c) Eusebius Ngemera 2015
 #-------------------------------------------------------------------------------
@@ -14,24 +13,40 @@ from Tkinter import *
 baseurl = "https://www.google.com/maps"
 
 def gui_main(n):
-    global root, variables, entries
+    global root, variables, entries, col_w
     root = Tk()
 
-    column_width = 50
-    Label(root, text="Start").grid(row=0,column=0)
-    Label(root, text="End").grid(row=0,column=1)
-    Button(root, text='Go!', width=20, command=open_maps).grid(row=0, column=2, sticky=W, pady=4)
-
+    col_w = 50
+    btn_w = 20
     variables = []
     entries = []
-    for i in range(2*n):
-        va = StringVar()
-        en = Entry(root, width=column_width, textvariable=va)
-        en.grid(row=(i/2)+1, column=i%2)
-        variables.append(va)
-        entries.append(en)
+
+    Label(root, text="Start").grid(row=0,column=0)
+    Label(root, text="End").grid(row=0,column=1)
+    Button(root, text='Go!', width=btn_w, command=open_maps).grid(row=0, column=2)
+    Button(root, text='Clear all', width=btn_w, command=clear_entries).grid(row=1, column=2)
+    Button(root, text='Add pair', width=btn_w, command=add_line).grid(row=2, column=2)
+
+    for unused in range(n):
+        add_line()
 
     root.mainloop()
+
+def clear_entries():
+    for e in entries:
+        e.delete(0, END)
+
+def add_line():
+    add_entry()
+    add_entry()
+
+def add_entry():
+    va = StringVar()
+    en = Entry(root, width=col_w, textvariable=va)
+    i = len(entries)
+    en.grid(row=(i/2)+1, column=i%2)
+    variables.append(va)
+    entries.append(en)
 
 def open_maps():
     for i in range(0, len(entries), 2):
